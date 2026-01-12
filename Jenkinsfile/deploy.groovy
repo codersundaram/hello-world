da7fd5a9d27246783683adb7b6428f3ac4a80935
Mohanasundaram <mohanasundaram.vlr@gmail.com>
2026-01-12 17:49:50 +0800

    updated groovy script

--- Jenkinsfile/deploy.groovy
+++ Jenkinsfile/deploy.groovy
@@ -1,14 +1,32 @@
-pipeline{
+pipeline {
     agent any
-    stages{
-        stage('Checkout'){
-            steps{
-                git 'https://github.com/codersundaram/hello-world.git'
+    tools {
+        maven 'mvn-3.9.12' // The name you gave in Global Tool Configuration
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/codersundaram/hello-world.git', branch: 'master'
             }
         }
         stage('Build') {
             steps {
-                sh 'mvn clean package'
+                bat 'mvn clean package'
+            }
+        }
+        stage('install') {
+            steps {
+                bat 'mvn clean install'
+            }
+        }
+        stage('docker-image') {
+            steps {
+                bat 'docker build -t springboot-app .'
+            }
+        }
+        stage('docker-container') {
+            steps {
+                bat 'docker run -p 8086:8080 springboot-app'
             }
         }
     }
