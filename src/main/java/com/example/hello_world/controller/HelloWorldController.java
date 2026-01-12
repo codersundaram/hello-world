7f6d705bde7534faa4bc93fd1596bffabcfd0676
Mohanasundaram <mohanasundaram.vlr@gmail.com>
2026-01-12 18:40:43 +0800

    modified message

--- src/main/java/com/example/hello_world/controller/HelloWorldController.java
+++ src/main/java/com/example/hello_world/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String getMessage(){
-        return "Hello-World";
+        return "Hello-World! Mohan here";
     }
 }
